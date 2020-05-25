c6b6ec01064ee6550a6fb07c22f31de94b4997e6
gerasimov <gerasimovku@gmail.com>
2020-05-24 18:51:32 -0700

    pushing messages to subscriber

--- FastQueue/FastQueue.Server.Core/Subscriber.cs
+++ FastQueue/FastQueue.Server.Core/Subscriber.cs
@@ -1,4 +1,5 @@
-﻿using FastQueue.Server.Core.Model;
+﻿using FastQueue.Server.Core.Exceptions;
+using FastQueue.Server.Core.Model;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,29 +10,58 @@ namespace FastQueue.Server.Core
     internal class Subscriber
     {
         private readonly Subscription subscription;
-        private readonly ISubscriber push;
+        private readonly Func<ReadOnlyMemory<Message>, Task> push;
 
-        public Subscriber(Subscription subscription, ISubscriber push)
+        private long sentMessageId;
+
+        public Subscriber(Subscription subscription, Func<ReadOnlyMemory<Message>, Task> push, long completedMessageId)
         {
             this.subscription = subscription;
             this.push = push;
+            sentMessageId = completedMessageId;
         }
 
-        internal async Task Push(ReadOnlyMemory<Message>[] data, int blockIndex, int indexInBlock)
+        internal async Task Push(ReadOnlyMemory<Message>[] data, long startMessageId)
         {
-            await push.Push(data[blockIndex].Span.Slice(indexInBlock));
-
-            for (int i = blockIndex + 1; i < data.Length; i++)
+            int blockInd = 0;
+            long blockStartMessageId = startMessageId;
+            bool dataSent = false;
+            long sentCount = 0;
+            while (blockInd < data.Length)
             {
-                await push.Push(data[i].Span);
+                var blockLen = data[blockInd].Length;
+
+                if (sentMessageId < blockStartMessageId + blockLen - 1)
+                {
+                    var memory = data[blockInd].Slice(checked((int)(sentMessageId - blockStartMessageId)) + 1);
+                    await push(memory);
+                    sentCount = memory.Length;
+                    dataSent = true;
+                    blockInd++;
+                    break;
+                }
+
+                blockInd++;
+                blockStartMessageId += blockLen;
+            }
+
+            while (blockInd < data.Length)
+            {
+                await push(data[blockInd]);
+                sentCount = data[blockInd].Length;
+                blockInd++;
+            }
+
+            if (dataSent)
+            {
+                long calculatedSentId = sentMessageId + sentCount;
+                sentMessageId = data[^1].Span[^1].ID;
+                // should never happen
+                if (sentMessageId != calculatedSentId)
+                {
+                    throw new FatalException($"Offset doesn't match ID");
+                }
             }
         }
     }
-
-    public interface ISubscriber
-    {
-        Task Push(ReadOnlySpan<Message> messages);
-    }
-
-
 }
--- FastQueue/FastQueue.Server.Core/Subscription.cs
+++ FastQueue/FastQueue.Server.Core/Subscription.cs
@@ -4,48 +4,39 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FastQueue.Server.Core
 {
     internal class Subscription
     {
-        private long offset;
+        private long completedMessageId;
         private Subscriber subscriber;
+        private int running;
 
-        internal void Push(ReadOnlyMemory<Message>[] data, int blockLength)
+        internal async Task Push(ReadOnlyMemory<Message>[] data, long startMessageId)
         {
-            var sb = subscriber;
-            if (sb == null)
+            try
             {
-                return;
-            }
+                if (Interlocked.CompareExchange(ref running, 1, 0) == 1)
+                {
+                    return;
+                }
 
-            if (data.Length == 0 || data[0].Length == 0
-                || offset - data[^1].Span[^1].ID == 1)
+                if (subscriber == null)
+                {
+                    return;
+                }
+
+                await subscriber.Push(data, startMessageId);
+            }
+            finally
             {
-                return;
+                Interlocked.Exchange(ref running, 0);
             }
-
-            int blockIndex;
-            long secondBlockStartID;
-            if (data.Length == 1 || (secondBlockStartID = data[1].Span[0].ID) > offset)
-            {
-                blockIndex = 0;
-            }
-            else
-            {
-                
-                blockIndex = checked((int)((offset - secondBlockStartID) / blockLength) + 1);
-            }
-
-            int indexInBlock = checked((int)(offset - data[blockIndex].Span[0].ID));
-
-            if (data[blockIndex].Span[indexInBlock].ID != offset)
-            {
-                throw new FatalException($"Offset doesn't match ID");
-            }
-
-            TaskHelper.FireAndForget(async () => await sb.Push(data, blockIndex, indexInBlock));
         }
+
+
     }
 }
